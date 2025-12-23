5c77ff0e7dc6e51cf8d9ae2d75bdc10495a4c71b
zezoloulah <zezoloula5@gmail.com>
2025-12-23 23:15:09 +0200

    first commit

---
+++ lib/features/splash/presentation/view/splash_Screen.dart
@@ -0,0 +1,13 @@
+import 'package:bookly/features/splash/presentation/view/widget/splash_view_body.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SplashViewBody(),
+    );
+  }
+}
